ac8baf49f24ff42f848b8a6679e5a33f2a341b72
alexeevtimothy-stack <alexeevtimothy@gmail.com>
2026-05-30 20:47:31 +0300

    Delete БазовыеТипыДанныхИЛитералы/Practic3.java

--- БазовыеТипыДанныхИЛитералы/Practic3.java
+++
@@ -1,9 +0,0 @@
-void main() {
-    String str1 = "AAA";
-    String str2 = "BBB";
-    int number = 100;
-    double pi = 3.14;
-    boolean status = true;
-    System.out.println(str1+str2);//конкатенация строки с разными типами данных и другими строками
-    System.out.println(pi + str1 +"+" + status);
-}
