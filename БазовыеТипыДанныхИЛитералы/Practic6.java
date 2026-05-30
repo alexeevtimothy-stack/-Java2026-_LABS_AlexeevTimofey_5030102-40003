119e021460eb1b36aa22d9075e2646f9cd6085a9
alexeevtimothy-stack <alexeevtimothy@gmail.com>
2026-05-30 20:59:07 +0300

    Практика 6

---
+++ БазовыеТипыДанныхИЛитералы/Practic6.java
@@ -0,0 +1,7 @@
+void main() {
+    double doubleVal = 999.99;
+    int intVal = (int) doubleVal;
+    byte byteVal = (byte) doubleVal;
+    System.out.println(intVal);//сужающее преобразование типов
+    System.out.println(byteVal);
+}
